e3d365f33a669e2a10e1f98ed6b1ea1c9e972dc4
ristes <ristes@gmail.com>
2019-10-17 12:32:36 +0200

    in memory room repository implementation

---
+++ web/src/main/java/mk/ukim/finki/wp/consultations/repository/inmemory/InMemoryRoomRepositoryImpl.java
@@ -0,0 +1,47 @@
+package mk.ukim.finki.wp.consultations.repository.inmemory;
+
+
+import mk.ukim.finki.wp.consultations.bootstrap.DataHolder;
+import mk.ukim.finki.wp.consultations.model.Room;
+import mk.ukim.finki.wp.consultations.repository.RoomRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Repository
+public class InMemoryRoomRepositoryImpl implements RoomRepository {
+
+    @Override
+    public Room save(Room room) {
+        this.findById(room.getName()).ifPresent(DataHolder.rooms::remove);
+        DataHolder.rooms.add(room);
+        return room;
+    }
+
+    @Override
+    public List<Room> findAll() {
+        return new ArrayList<>(DataHolder.rooms);
+    }
+
+    @Override
+    public List<Room> searchRooms(String term) {
+        return DataHolder.rooms.stream()
+                .filter(r -> r.getName().contains(term) || r.getDescription().contains(term) || r.getBuilding().getDescription().contains(term))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<Room> findById(String name) {
+        return DataHolder.rooms.stream()
+                .filter(r -> r.getName().equals(name))
+                .findFirst();
+    }
+
+    @Override
+    public void deleteById(String name) {
+        this.findById(name).ifPresent(DataHolder.rooms::remove);
+    }
+}
