c15b8b413ec279c7bb49e63966c1d09f9513c94c
kokimishev <kokimisev@gmail.com>
2019-10-27 02:25:33 +0100

    adding room service dependency into web servlet

--- web/src/main/java/mk/ukim/finki/wp/consultations/servlets/RoomWebServlet.java
+++ web/src/main/java/mk/ukim/finki/wp/consultations/servlets/RoomWebServlet.java
@@ -1,5 +1,8 @@
 package mk.ukim.finki.wp.consultations.servlets;
 
+import mk.ukim.finki.wp.consultations.model.Room;
+import mk.ukim.finki.wp.consultations.service.RoomService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,16 +10,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet(name="room-web-servlet",urlPatterns = "/servlet/room")
 public class RoomWebServlet extends HttpServlet {
+
+    private RoomService roomService;
+
+    public RoomWebServlet(RoomService roomService) {
+        this.roomService = roomService;
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         String ipAddress = req.getRemoteHost();
         String clientAgent = req.getHeader("User-Agent");
+        resp.setContentType("text/html; charset=UTF-8");
         PrintWriter writer = resp.getWriter();
+        List<Room> rooms = roomService.getAllRooms();
         writer.write("<html>");
         writer.write("<head>");
         writer.write("</head>");
@@ -28,6 +40,13 @@ public class RoomWebServlet extends HttpServlet {
         writer.write("Your IP address is:");
         writer.write(ipAddress);
         writer.write("</div>");
+        writer.write("<div>Let's list the rooms</div>");
+        writer.write("<ul>");
+        rooms.stream()
+                .forEach(r->
+                        writer.write("<li>"+r.getName()+"</li>")
+        );
+        writer.write("</ul>");
         writer.write("</body>");
         writer.write("</html>");
 
