f9a9284af437f7d62fe1817bef2f49130e77dd30
kokimishev <kokimisev@gmail.com>
2019-10-27 12:02:39 +0100

    wiring Thymeleaf with http servlet

--- web/src/main/java/mk/ukim/finki/wp/consultations/ConsultationsApplication.java
+++ web/src/main/java/mk/ukim/finki/wp/consultations/ConsultationsApplication.java
@@ -1,12 +1,40 @@
 package mk.ukim.finki.wp.consultations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @SpringBootApplication
 @ServletComponentScan
 public class ConsultationsApplication {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    final SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+
+    private SpringResourceTemplateResolver templateResolver() {
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setCacheable(false);
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine getSpringTemplateEngine() {
+        final SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.addTemplateResolver(templateResolver());
+        return springTemplateEngine;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConsultationsApplication.class, args);
