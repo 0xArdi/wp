56ded66ca79dfcb9831375bcdd1ae3beaf26d9b7
ristes <ristes@gmail.com>
2019-10-29 16:09:34 +0100

    Removed the configuration's redundant field

--- web/src/main/java/mk/ukim/finki/wp/consultations/config/ThymeleafConfiguration.java
+++ web/src/main/java/mk/ukim/finki/wp/consultations/config/ThymeleafConfiguration.java
@@ -1,6 +1,5 @@
 package mk.ukim.finki.wp.consultations.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,18 +8,17 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
-public class ThymeleafConfiguration  {
+public class ThymeleafConfiguration {
 
-    private ApplicationContext applicationContext;
+    private final ApplicationContext applicationContext;
 
     public ThymeleafConfiguration(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
     }
 
-    final SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-
     private SpringResourceTemplateResolver templateResolver() {
-        templateResolver.setApplicationContext(applicationContext);
+        final SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(this.applicationContext);
         templateResolver.setCacheable(false);
         templateResolver.setPrefix("classpath:/templates/");
         templateResolver.setSuffix(".html");
