4ce1a2a498ff181f7507682d8becf0b92ff99251
Ardian <ardianabazi09@gmail.com>
2019-11-05 08:33:02 +0100

    created impl for repo and service, refactored impl of PizzaRepo

--- lab/src/main/java/mk/finki/ukim/mk/lab/repository/impl/PizzaInMemoryRepository.java
+++ lab/src/main/java/mk/finki/ukim/mk/lab/repository/impl/PizzaInMemoryRepository.java
@@ -1,7 +1,8 @@
-package mk.finki.ukim.mk.lab.repository;
+package mk.finki.ukim.mk.lab.repository.impl;
 
 import mk.finki.ukim.mk.lab.bootstrap.DataHolder;
 import mk.finki.ukim.mk.lab.model.Pizza;
+import mk.finki.ukim.mk.lab.repository.PizzaRepository;
 
 import java.util.List;
 
