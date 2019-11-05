b41d3160bf92bda82f2fbfc75946fdb7f8e77db1
Ardian <ardianabazi09@gmail.com>
2019-11-05 08:40:07 +0100

    Implemented pizza service

--- lab/src/main/java/mk/finki/ukim/mk/lab/repository/impl/PizzaInMemoryRepository.java
+++ lab/src/main/java/mk/finki/ukim/mk/lab/repository/impl/PizzaInMemoryRepository.java
@@ -3,9 +3,11 @@ package mk.finki.ukim.mk.lab.repository.impl;
 import mk.finki.ukim.mk.lab.bootstrap.DataHolder;
 import mk.finki.ukim.mk.lab.model.Pizza;
 import mk.finki.ukim.mk.lab.repository.PizzaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class PizzaInMemoryRepository implements PizzaRepository {
     @Override
     public List<Pizza> getAllPizzas() {
