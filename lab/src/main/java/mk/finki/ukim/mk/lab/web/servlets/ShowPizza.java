25fe78272f7671e63571cfee53dc54094b60c8f6
Ardian <ardianabazi09@gmail.com>
2019-11-06 13:30:59 +0100

    fully implemented show pizza

--- lab/src/main/java/mk/finki/ukim/mk/lab/web/servlets/ShowPizza.java
+++ lab/src/main/java/mk/finki/ukim/mk/lab/web/servlets/ShowPizza.java
@@ -1,18 +1,35 @@
 package mk.finki.ukim.mk.lab.web.servlets;
 
+import mk.finki.ukim.mk.lab.model.Pizza;
+import mk.finki.ukim.mk.lab.service.PizzaService;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet()
+@WebServlet(urlPatterns = "")
 public class ShowPizza extends HttpServlet {
+
+    private final PizzaService pizzaService;
+    private final SpringTemplateEngine springTemplateEngine;
+
+    public ShowPizza(PizzaService pizzaService, SpringTemplateEngine springTemplateEngine) {
+        this.pizzaService = pizzaService;
+        this.springTemplateEngine = springTemplateEngine;
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().print("Mirdita");
-        resp.getWriter().flush();
+        WebContext webContext = new WebContext(req, resp, req.getServletContext());
+        List<Pizza> pizzas = this.pizzaService.listPizzas();
+        webContext.setVariable("pizzas", pizzas);
+        resp.setContentType("text/html; charset=UTF-8");
+        this.springTemplateEngine.process("show-pizza.html", webContext, resp.getWriter());
     }
 }
