cd6c4630a880343626850412c3b2851e295a14f1
Ardian <ardianabazi09@gmail.com>
2019-11-27 19:28:26 +0100

    removed in-memory implementation of pizza repo

--- lab/src/main/java/mk/finki/ukim/mk/lab/repository/PizzaRepository.java
+++ lab/src/main/java/mk/finki/ukim/mk/lab/repository/PizzaRepository.java
@@ -4,9 +4,7 @@ import mk.finki.ukim.mk.lab.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PizzaRepository extends JpaRepository<Pizza, String> {
-//    List<Pizza> getAllPizzas();
+    
 }
